f85a0d6c8b9d6fbd4b3a8bfdc6d7f0155573bc75
MuuSeoTia <tiahimouad22@gmail.com>
2024-10-19 12:24:32 -0700

    Updated and retrieved model information

--- workers/dummy.py
+++ workers/dummy.py
@@ -1,3 +1,5 @@
+
+
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
@@ -5,15 +7,22 @@ import numpy as np
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (_, _), (x_test, y_test) = fashion_mnist.load_data()
 
-def preprocess_images(images):
+def preprocess_images(images, labels):
+    print("Processing images with shape:", images.shape)
+    print("Corresponding labels shape:", labels.shape)
+    print("First 5 labels:", labels[:5])  
+
     images = np.expand_dims(images, axis=-1)
     images = tf.image.resize(images, [224, 224]).numpy()
+
     images = images / 255.0
-    return images
 
-x_test = preprocess_images(x_test)
+    return images, labels 
 
-x_test_rgb = np.repeat(x_test, 3, axis=-1)  
+x_test, y_test = preprocess_images(x_test, y_test)
+
+x_test_rgb = np.repeat(x_test, 3, axis=-1)
+
 
 model = tf.keras.applications.MobileNetV2(
     input_shape=(224, 224, 3),
@@ -24,7 +33,6 @@ model = tf.keras.applications.MobileNetV2(
 model_id = id(model)
 num_layers = len(model.layers)
 
-
 class ModelShard:
     def __init__(self, model_id, num_layers, ground_truth):
         self.model_id = model_id
@@ -32,18 +40,19 @@ class ModelShard:
         self.ground_truth = ground_truth  
 
     def __repr__(self):
-        return f"ModelShard(model_id={self.model_id}, num_layers={self.num_layers})"
+        return f"ModelShard(model_id={self.model_id}, num_layers={self.num_layers}, ground_truth_shape={self.ground_truth.shape})"
 
 
 model_shard = ModelShard(model_id, num_layers, y_test)
-print(model_shard)  
+print(model_shard) 
 
-model.export('saved_model')
+# modular api
+#model.export('saved_model')
+
+#converter = tf.lite.TFLiteConverter.from_saved_model('saved_model')
+#tflite_model = converter.convert()
 
 
-converter = tf.lite.TFLiteConverter.from_saved_model('saved_model')
-tflite_model = converter.convert()
-
-with open('mobilenetv2.tflite', 'wb') as f:
-    f.write(tflite_model)
-print("TensorFlow Lite model saved as 'mobilenetv2.tflite'")
+#with open('mobilenetv2.tflite', 'wb') as f:
+    #f.write(tflite_model)
+#print("TensorFlow Lite model saved as 'mobilenetv2.tflite'")
