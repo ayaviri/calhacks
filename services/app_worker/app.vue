86baa3de2c64998d82d8a2266b25cc95d68fb23c
Kenneth Chap <53070266+Zapaway@users.noreply.github.com>
2024-10-19 19:15:24 -0700

    :sparkles: added stomp

--- services/app_worker/app.vue
+++ services/app_worker/app.vue
@@ -1,16 +1,46 @@
 <script setup lang="js">
-import amqp from 'amqplib/callback_api';
+import { Stomp } from 'stompjs';
 
-const isWorker = useState("isWorker", () => false);
-const requestToBeWorker = async (_) => {
-  isWorker.value = true;
-}
+const WebSocket = window.__TAURI__.websocket;
 
+const socket = new WebSocket('ws://localhost:15674/ws'); // Use RabbitMQ's WebSocket URL
+const client = Stomp.over(socket);
+
+// const REQUEST_ENDPOINT = 'amqp://localhost';
+
+// const isWorker = useState("isWorker", () => false);
+// const requestToBeWorker = async (_) => {
+//   isWorker.value = true;
+//   // 1) Request the core server to add this phone to the network
+//   response = null
+//   rabbitmqUrl = (await response.json()).rabbitmq_url
+//   amqp.connect(rabbitmqUrl, function(error0, connection) {
+//   if (error0) {
+//     throw error0;
+//   }
+//   connection.createChannel(function(error1, channel) {
+//     if (error1) {
+//       throw error1;
+//     }
+//     var queue = 'subtask';
+
+//     channel.assertQueue(queue, {
+//       durable: false
+//     });
+//   });
+//   channel.consume(queue, function(msg) {
+//   console.log(" [x] Received %s", msg.content.toString());
+//   }, {
+//     noAck: true
+//   });
+// });
+// }
 </script>
 
 <template>
   <div>
+    <p>Hello</p>
     <button @click="requestToBeWorker">Click me!</button>
-    <p>{{ isWorker ? "not worker" : "worker" }}</p>
+    <p>{{ isWorker ? "not worker" : "worker!" }}</p>
   </div>
 </template>
